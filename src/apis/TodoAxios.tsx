1fca510cf0d8edf75adec4cb4097d2e8f18ff747
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-06-29 03:05:13 +0900

    Feat: 회원가입, 로그인, 투두 api 추가

---
+++ src/apis/TodoAxios.tsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+// axios 생성
+export const todoAxios = axios.create({
+  baseURL: "https://www.pre-onboarding-selection-task.shop/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// update Todo List API 연결
+export async function updateTodoAxios(id: number, todo: string, isCompleted: boolean) {
+  try {
+    const res = await todoAxios.put(`/todos/${id}`, {
+      todo: todo,
+      isCompleted: isCompleted,
+    });
+    if (res.status === 200) return res;
+  } catch (error: any) {
+    return error;
+  }
+}
+
+// delete Todo List API 연결
+export async function deleteTodoAxios(id: number) {
+  try {
+    const res = await todoAxios.delete(`/todos/${id}`);
+    if (res.status === 204) {
+      const res = await getTodosList();
+      return res;
+    }
+  } catch (error: any) {
+    return error;
+  }
+}
+
+// get Todo List API 연결
+export async function getTodosList() {
+  try {
+    const res = await todoAxios.get("/todos");
+    if (res.status === 200) {
+      return res;
+    }
+  } catch (error: any) {
+    return error;
+  }
+}
+
+// create Todo List API 연결
+export async function createTodosList(updateValue: string) {
+  try {
+    const res = await todoAxios.post("/todos", {
+      todo: updateValue,
+    });
+    if (res.status === 201) return true;
+  } catch (error: any) {
+    return error;
+  }
+}
