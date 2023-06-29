a5289d05f660ab65530c6e70240b7628b836446c
codingjwp <codingjwp@gmail.com>
2023-06-29 12:26:50 +0900

    Create: Home.tsx 파일 생성 Code: button 클릭스 navigate 해당 페이지로 이동하는 기능 Design: 임시 HomeWrapper을 만들어서 작성

---
+++ src/components/todo/TodoCreate.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const TodoCreate = () => {
+  return <div>create</div>;
+};
+
+export default TodoCreate;
---
+++ src/components/todo/TodoModify.tsx
@@ -0,0 +1,5 @@
+const TodoModify = () => {
+  return <div>Modify</div>;
+};
+
+export default TodoModify;
