818760aa26d2c656e5585c707cf0b125361c3f80
KimEunHye <gpqjr00@naver.com>
2023-06-29 11:13:36 +0900

    feat:유효성 검증 훅 추가

--- src/hooks/useValidation.tsx
+++ src/hooks/useValidation.tsx
@@ -1,9 +1,61 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// interface Email {}
+interface EmailStatus {
+  message: string;
+  status: boolean;
+}
 
-const useValidation = () => {
-  return;
+interface PasswordStatus {
+  message: string;
+  status: boolean;
+}
+
+const useValidation = (target: { email: string; password: string }) => {
+  const { email, password } = target;
+
+  const [emailStatus, setEmailStatus] = useState<EmailStatus>({
+    message: '',
+    status: false,
+  });
+
+  const [passwordStatus, setPasswordStatus] = useState<PasswordStatus>({
+    message: '',
+    status: false,
+  });
+
+  useEffect(() => {
+    const emailValid = email.includes('@');
+
+    if (!emailValid) {
+      setEmailStatus({
+        message: '이메일에 @를 포함시켜주세요.',
+        status: false,
+      });
+    } else {
+      setEmailStatus({
+        message: '',
+        status: true,
+      });
+    }
+  }, [email]);
+
+  useEffect(() => {
+    const passwordValid = password.length > 7;
+
+    if (!passwordValid) {
+      setPasswordStatus({
+        message: '비밀번호는 8자 이상이여야 합니다.',
+        status: false,
+      });
+    } else {
+      setPasswordStatus({
+        message: '',
+        status: true,
+      });
+    }
+  }, [password]);
+
+  return [emailStatus, passwordStatus];
 };
 
 export default useValidation;
