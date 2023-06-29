4996bd57263951323f62f4f1295b5452d0d45614
rondido <rppr001@naver.com>
2023-06-29 18:36:47 +0900

    Create: component 폴더안 Todo Component 생성

---
+++ src/components/Todo/TodoItemGroup.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoItemGroup() {
+  return <div>TodoItemGroup</div>;
+}
---
+++ src/components/Todo/TodoTrans.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoTrans() {
+  return <div>TodoTrans</div>;
+}
