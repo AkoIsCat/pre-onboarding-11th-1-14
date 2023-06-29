72dd5612e9e46279c6d6ddd0346063af271b324d
rondido <rppr001@naver.com>
2023-06-29 19:02:25 +0900

    Feat: Todo 조회 삭제 수정상태 구현(api 미구현으로 인한 error 발생)

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+//data를 get으로 받아온 값을 다시 TodoItem 컴포넌트로 넘겨준다.
+interface TodoProps {
+  id: string;
+  todo: string;
+  isCompleted: boolean;
+  userId: string;
+  setbuttonStatus: (active: boolean) => void;
+  updateCheckTodo: (id: string, todo: string, isCompleted: boolean) => void;
+  deletebutton: (id: string) => void;
+}
 
-export default function todoItem() {
-  return <div>todoItem</div>;
+export default function TodoItem({
+  id,
+  todo,
+  isCompleted,
+  updateCheckTodo,
+  setbuttonStatus,
+  deletebutton,
+}: TodoProps) {
+  return (
+    <div>
+      <ul>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              checked={isCompleted}
+              onChange={() => updateCheckTodo(id, todo, isCompleted)}
+            />
+            <span id={id}>{todo}</span>
+          </label>
+          <button
+            data-testid="modify-button"
+            onClick={() => setbuttonStatus(true)}
+          >
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={() => deletebutton(id)}>
+            삭제
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/components/Todo/TodoTrans.tsx
+++ src/components/Todo/TodoTrans.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { useState } from "react";
 
-export default function TodoTrans() {
-  return <div>TodoTrans</div>;
+import TodoItem from "./TodoItem";
+
+interface TodoProps {
+  id: string;
+  todo: string;
+  isCompleted: boolean;
+  userId: string;
+  updateCheckTodo: (
+    id: string,
+    todo: string,
+    isCompleted: boolean
+  ) => Promise<void>;
+  deletebutton: (id: string) => void;
+}
+
+export default function TodoTrans({
+  id,
+  todo,
+  isCompleted,
+  userId,
+  updateCheckTodo,
+  deletebutton,
+}: TodoProps) {
+  const [buttonStatus, setbuttonStatus] = useState<Boolean>(false);
+  return (
+    <div>
+      {!buttonStatus ? (
+        <TodoItem
+          id={id}
+          todo={todo}
+          isCompleted={isCompleted}
+          userId={userId}
+          setbuttonStatus={setbuttonStatus}
+          updateCheckTodo={updateCheckTodo}
+          deletebutton={deletebutton}
+        />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
